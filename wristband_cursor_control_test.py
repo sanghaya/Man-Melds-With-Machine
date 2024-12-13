e872864b93a2c423d1ed79bf5f667ea12f8792cf
JBKC <jmbannon97@gmail.com>
2024-12-13 22:37:37 +0000

    basic mouse movement commands

--- wristband_cursor_control_test.py
+++ wristband_cursor_control_test.py
@@ -8,53 +8,44 @@ import serial
 import asyncio
 from pynput.mouse import Controller, Button
 import time
-
-# Create a mouse controller object
-mouse = Controller()
-
-# Move the cursor to a specific position (x, y)
-mouse.position = (500, 300)
-print(f"Moved to {mouse.position}")
-
-# Wait for 1 second
-time.sleep(1)
-
-# Move the cursor relatively
-mouse.move(100, 50)
-print(f"New position: {mouse.position}")
-
-# Perform a left click
-mouse.click(Button.left, 1)
-
-# Perform a double click
-mouse.click(Button.left, 2)
-
-# Perform a right click
-mouse.click(Button.right, 1)
-
-# Scroll up
-mouse.scroll(0, 2)
-
-# Scroll down
-mouse.scroll(0, -2)
 import collections
 
-async def producer(ser):
-    """
-    Parses streaming data and appends to a buffer (sliding window).
-    """
+async def producer(ser, buffer):
 
     print(f'Streaming data....')
 
     while True:
-        if ser.in_waiting > 0:
+        try:
             packet = ser.readline().decode('utf-8').strip()
             parts = packet.split(',')
             timestamp = float(parts[0])
-            accel = tuple((float(parts[1]), float(parts[2]), float(parts[3])))
-            print(parts)
+            acc = tuple((float(parts[1]), float(parts[2]), float(parts[3])))
 
-            await asyncio.sleep(0)
+            buffer.append(acc)
+
+        except Exception as e:
+            print(f"Error reading data: {e}")
+
+        await asyncio.sleep(0)
+
+async def consumer(buffer, mouse):
+    '''
+    Simple mouse movements
+    '''
+
+    while True:
+        x,y,z = buffer[-1]
+        print(y)
+        if x > 0.2:
+            mouse.move(dx=1, dy=0)
+        if x < -0.2:
+            mouse.move(dx=-1, dy=0)
+        if y > 0.2:
+            mouse.move(dx=0, dy=1)
+        if y < -0.2:
+            mouse.move(dx=0, dy=-1)
+
+        await asyncio.sleep(0)
 
 async def main():
     '''
@@ -73,12 +64,18 @@ async def main():
         print(f"Failed to connect to the Bluetooth device: {e}")
         return
 
+    buffer = collections.deque(maxlen=100)                    # buffer for storing accelerometer inputs
+
+    # Create a mouse controller object
+    mouse = Controller()
+
     # extract & process data concurrently
     async with asyncio.TaskGroup() as tg:
-        task1 = tg.create_task(producer(ser))
+        task1 = tg.create_task(producer(ser, buffer))
+        task2 = tg.create_task(consumer(buffer, mouse))
 
     # run tasks
-    await asyncio.gather(task1)
+    await asyncio.gather(task1, task2)
 
 
 if __name__ == '__main__':
